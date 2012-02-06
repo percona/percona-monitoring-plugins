c4fb5f6dfcf8aef2fa019e1b13f336a9b2309ed9
baron@percona.com <>
2012-02-06 09:44:46 -0500

    tests for innodb-status plugin and processlist plugin

---
+++ t/nagios/pmp-check-mysql-innodb/check-functions.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+. ../../../nagios/pmp-check-mysql-innodb
+
+echo "should print 40"
+get_longest_trx "samples/innodb-status-001.txt"
+
+echo "should print 0"
+get_waiter_count "samples/innodb-status-001.txt"
+
+echo "should print 1"
+get_waiter_count "samples/innodb-status-002.txt"
---
+++ t/nagios/pmp-check-mysql-processlist/check-functions.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+. ../../../nagios/pmp-check-mysql-processlist
+
+echo "should print 15"
+max 1 5 15 11 8
+
+echo "should print 1"
+count_mysql_processlist "samples/locked-processlist-5.1-1.txt" \
+   "State" "Locked"
