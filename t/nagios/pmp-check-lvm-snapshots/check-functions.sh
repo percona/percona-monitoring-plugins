6edefdbe23eb3006ca0cde8b1529e1f16fdea188
baron@percona.com <>
2012-02-19 21:39:24 -0500

    Make the Nagios plugins more testable (fixes bug #936579)

--- t/nagios/pmp-check-lvm-snapshots/check-functions.sh
+++ t/nagios/pmp-check-lvm-snapshots/check-functions.sh
@@ -16,3 +16,6 @@ check_lvm_snapshot_fullness "samples/lvs-003.txt" 20
 
 echo "should print nothing"
 check_lvm_snapshot_fullness "samples/lvs-004.txt" 30
+
+echo "should print CRIT LVM snapshot volumes over 20% full: vg2/lv2[lv1]=20.57%"
+main -c 20 "samples/lvs-003.txt"
