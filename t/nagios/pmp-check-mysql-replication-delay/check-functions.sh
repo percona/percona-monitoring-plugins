c73bdcdb48eae31e82acdea3c3e97561465f7b6b
Brian Fraser <brian.fraser@percona.com>
2012-08-23 12:21:28 -0300

    Fix for 1040528: pmp-check-mysql-replication-delay doesn't alert if second behing master is NULL

--- t/nagios/pmp-check-mysql-replication-delay/check-functions.sh
+++ t/nagios/pmp-check-mysql-replication-delay/check-functions.sh
@@ -10,3 +10,11 @@ main -w 9 samples/show-slave-status-001.txt
 
 echo "should print CRIT 10 seconds of replication delay"
 main -c 9 samples/show-slave-status-001.txt 
+
+echo "should print UNK if the slave is not running"
+out=`main samples/show-slave-status-002.txt`
+if [ "${out:-""}" = "UNK slave is not running" ]; then
+   echo "ok"
+else
+   echo "not ok"
+fi
