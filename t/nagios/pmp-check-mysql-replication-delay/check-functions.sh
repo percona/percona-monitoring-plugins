8803ae02b1abc70c423c871f22f9a8d3c2cb60b0
Roman Vynar <roman.vynar@percona.com>
2013-03-11 14:25:41 +0200

    Fix for bug #1103364: pmp-check-mysql-replication-delay incompatible with latest pt-hearbeat 2.1.8

--- t/nagios/pmp-check-mysql-replication-delay/check-functions.sh
+++ t/nagios/pmp-check-mysql-replication-delay/check-functions.sh
@@ -12,9 +12,4 @@ echo "should print CRIT 10 seconds of replication delay"
 main -c 9 samples/show-slave-status-001.txt 
 
 echo "should print UNK if the slave is not running"
-out=`main samples/show-slave-status-002.txt`
-if [ "${out:-""}" = "UNK replica is stopped" ]; then
-   echo "ok"
-else
-   echo "not ok"
-fi
+main samples/show-slave-status-002.txt
