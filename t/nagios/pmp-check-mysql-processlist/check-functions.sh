fdddffbba9b7f32749a760487b22f0b3a6519550
Roman Vynar <roman.vynar@percona.com>
2013-07-04 13:00:01 +0300

    Fix for bug #1197084 pmp-check-mysql-processlist: "copy to table" counter always 0

--- t/nagios/pmp-check-mysql-processlist/check-functions.sh
+++ t/nagios/pmp-check-mysql-processlist/check-functions.sh
@@ -8,3 +8,11 @@ max 1 5 15 11 8
 echo "should print 1"
 count_mysql_processlist "samples/locked-processlist-5.1-1.txt" \
    "State" "Locked"
+
+echo "should print 2"
+count_mysql_processlist "samples/locked-processlist-5.5-1.txt" \
+   "State" "Waiting for table level lock"
+
+echo "should print 4"
+count_mysql_processlist "samples/copy-processlist-1.txt" \
+   "State" ".*opy.* to.* table.*"
