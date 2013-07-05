7a9225df9825b69c09e71abcc37bb74a5def3471
Roman Vynar <roman.vynar@percona.com>
2013-07-05 12:19:01 +0300

    Fix for bug #1191305 Comparison of status variables that are strings doesn't work as expected

--- t/nagios/pmp-check-mysql-status/check-functions.sh
+++ t/nagios/pmp-check-mysql-status/check-functions.sh
@@ -2,47 +2,76 @@
 
 . ../../../nagios/bin/pmp-check-mysql-status
 
-echo "should print 1"
+printf "should print 1: "
 compute_result samples/status-variables-001.txt Threads_running
 
-echo "should print 0.150531"
+printf "should print 0.150531: "
 compute_result samples/status-variables-001.txt Bytes_received / Bytes_sent
 
-echo "should print 15.053114"
+printf "should print 15.053114: "
 compute_result samples/status-variables-001.txt Bytes_received / Bytes_sent pct
 
-echo "should exit 0"
+printf "should exit 0: "
 compare_result 15 20 30 '>='
 echo $?
 
-echo "should exit 1"
+printf "should exit 1: "
 compare_result 15 20 15 '>='
 echo $?
 
-echo "should exit 0"
+printf "should exit 0: "
 compare_result 15 20 15 '>'
 echo $?
 
-echo "should exit 2"
+printf "should exit 2: "
 compare_result 15 15 10 '>='
 echo $?
 
-echo "should exit 0"
+printf "should exit 0: "
 compare_result 15 "" "" '>='
 echo $?
 
-echo "should exit 0"
+printf "should exit 0: "
 compare_result 15 "20" "" '>='
 echo $?
 
-echo "should exit 0"
+printf "should exit 0: "
 compare_result 15 "" "20" '>='
 echo $?
 
-echo "should exit 1"
+printf "should exit 1: "
 compare_result 2 "" 4 '!='
 echo $?
 
-echo "should exit 0"
+printf "should exit 0: "
 compare_result 4 "" 4 '!='
 echo $?
+
+printf "should exit 1: "
+compare_result 4 "" 4 '=='
+echo $?
+
+printf "should exit 2: "
+compare_result STRICT IDEMPOTENT "" '=='
+echo $?
+
+printf "should exit 0: "
+compare_result STRICT IDEMPOTENT "" '==' str
+echo $?
+
+printf "should exit 1: "
+compare_result STRICT "" STRICT '==' str
+echo $?
+
+printf "should exit 0: "
+compare_result STRICT STRICT "" '!=' str
+echo $?
+
+printf "should exit 0: "
+compare_result 5 20 "" '>='
+echo $?
+
+printf "should exit 2: "
+compare_result 5 20 "" '>=' str
+echo $?
+
