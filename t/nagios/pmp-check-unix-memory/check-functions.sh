b4636c49f0188d2af1235293d01396abeddacf7f
Roman Vynar <roman.vynar@percona.com>
2015-12-28 16:18:08 +0200

    Fixed pmp-check-unix-memory for EL7, using MemAvailable is available

--- t/nagios/pmp-check-unix-memory/check-functions.sh
+++ t/nagios/pmp-check-unix-memory/check-functions.sh
@@ -2,8 +2,11 @@
 
 . ../../../nagios/bin/pmp-check-unix-memory
 
-echo "Should print 23"
-get_used_memory_linux  samples/free-001.txt
+echo "Should print 29"
+get_used_memory_linux  samples/meminfo-001.txt
+
+echo "Should print 89"
+get_used_memory_linux  samples/meminfo-002.txt
 
 echo "Should print 53"
 get_used_memory_bsd  samples/sysctl-001.txt
