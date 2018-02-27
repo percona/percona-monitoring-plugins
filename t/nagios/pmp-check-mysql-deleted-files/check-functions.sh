5238bdb58c130ce89e1c7f1df290a286b7d2d764
Brendan Byrd <brendan.byrd@grantstreet.com>
2018-02-27 13:22:32 -0500

    pmp-check-mysql-deleted-files: Add a unit test for --exclude

--- t/nagios/pmp-check-mysql-deleted-files/check-functions.sh
+++ t/nagios/pmp-check-mysql-deleted-files/check-functions.sh
@@ -7,3 +7,7 @@ check_deleted_files samples/lsof-001.txt /tmp/
 
 echo "should print out 5 /tmp/ib* files"
 check_deleted_files samples/lsof-001.txt /foo/
+
+# regular expression with part of the tmp directory and part of the ib* filename
+echo "should print out 2 /tmp/ib* files"
+check_deleted_files samples/lsof-001.txt p/ib[Vsr].+
